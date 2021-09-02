fb988b64114e23e586f943692f985547d0388863
Dylan Hoffman <jkioicefire.gmail.com>
2021-09-02 07:49:44 -0500

    added an under construction component

---
+++ src/components/UI_Sections/Construction.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Construction = () => {
+
+    return(
+        <React.Fragment>
+            <h1>This Website is still under construction</h1>
+            <h2>Be sure to check out my Facebook here!</h2>
+            <a href="https://www.facebook.com/Radiantbronzingboutique/">Radiant Bronzing Boutique LLC</a>
+        </React.Fragment>
+    );
+}
+
+
+export default Construction;
